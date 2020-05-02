8847af054b82c2206e88f228b8959e4fd71bf4bb
Zemian Deng <zemian.deng@oracle.com>
2020-05-02 15:12:58 -0400

    Init spring-quartz

---
+++ src/test/java/zemian/springquartz/SpringQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package zemian.springquartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuartzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
