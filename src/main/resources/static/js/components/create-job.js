337566e017d57ecc25210c1118aa3ab85f9d027a
Zemian Deng <zemian.deng@oracle.com>
2020-05-03 19:06:28 -0400

    Add create-job page

--- src/main/resources/static/js/components/create-job.js
+++ src/main/resources/static/js/components/create-job.js
@@ -3,6 +3,43 @@ define(['text!./create-job.html'], function(html) {
         template: html,
         data: function () {
             return {
+                baseUrl: "http://localhost:8080",
+                jobName: 'MyJob-' + Math.random().toString(16).substring(2),
+                jobClassName: 'zemian.springquartz.LoggerJob',
+                repeatCount: 1,
+                repeatInterval: 60000,
+                createdJobKey: null
+            }
+        },
+        methods: {
+            createJob: function () {
+                // Generate some fake jobs
+                let self = this;
+                let url = this.baseUrl + "/jobs";
+                let sendData = {
+                    jobDetail: {
+                        name: self.jobName,
+                        jobClassName: self.jobClassName
+                    },
+                    trigger: {
+                        repeatCount: self.repeatCount,
+                        repeatInterval: self.repeatInterval
+                    }
+                };
+                fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(sendData)
+                }).then(resp => resp.json().then(data => {
+                    console.log('Job created ', data);
+                    this.createdJobKey = data;
+                }));
+            },
+            resetNewJob: function () {
+                this.createdJobKey = null;
+                this.jobName = 'MyJob-' + Math.random().toString(16).substring(2);
             }
         }
     };
